e57ca920ee9141ad3f508f99c3aeabeb6f930633
Rathi <github email address>
2017-05-17 15:37:02 -0400

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -6,9 +6,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users
+  get  'static_pages/home'
+  get  'static_pages/help'
   get  '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  root 'users#new'
+  root 'static_pages#home'
 end
