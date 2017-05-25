cc5fd120da185571332f93db8edb99e311d1bf5f
Rathi <github email address>
2017-05-25 19:18:05 -0400

    included background img

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-
-  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :name, presence: true, length: { maximum: 50 }
+  validates :name, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 
   has_many :posts, dependent: :destroy
