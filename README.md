ec11b8ce3b6ae3dc2278f3c161266f1bd78280c6
Rathi <github email address>
2017-06-01 11:17:13 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # README
+
+Instagram inspired rails app,
+
+Technologies used:
+.Ruby
+.Ruby on Rails
+.Postgresql
+.Paperclip
+.Bootstrap
+.Git
+.Heroku
+
+How to run it
+
+git clone git@github.com:rathi2016/Insta_photo_gram.git
+cd Insta_photo_gram
+rails s
+
+open your browser on localhost:3000
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,9 @@ Rails.application.routes.draw do
   resources :sessions
   resources :users do
     resources :posts, only: [:index, :show, :edit, :update]
-
   end
 
   get  '/home',   to: 'static_pages#home'
-  get  'static_pages/help'
   get  '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
